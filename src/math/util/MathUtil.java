8a869ecd38b902ed21447481c45629843fabc951
dreck-nguyen <hduy01012000@gmail.com>
2021-03-16 17:28:17 +0700

    1st upload git add *!

---
+++ src/math/util/MathUtil.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package math.util;
+
+/**
+ *
+ * @author hduy0
+ */
+public class MathUtil {
+
+    public static void main(String[] args) {
+        System.out.println("1st upload from the Local to GitHub !!!");
+        System.out.println("Welcome to GitHub");
+    }
+    
+}
