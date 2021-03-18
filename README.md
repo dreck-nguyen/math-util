59c9d1a9ba4bd2c8fa46a2a32d876af2ea50e47b
dreck-nguyen <hduy01012000@gmail.com>
2021-03-18 15:15:53 +0700

    2nd updoaded

---
+++ README.md
@@ -0,0 +1,2 @@
+## Welcome to Math-Util repository. This repo offers various math 
+method that is your projects as the helper ones
--- src/math/util/MathUtil.java
+++ src/math/util/MathUtil.java
@@ -5,15 +5,83 @@
  */
 package math.util;
 
+import java.util.Objects;
+
 /**
  *
  * @author hduy0
  */
 public class MathUtil {
 
-    public static void main(String[] args) {
-        System.out.println("1st upload from the Local to GitHub !!!");
-        System.out.println("Welcome to GitHub");
+    public Long getId() {
+        return id;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    private Long id;
+    private String username;
+    private String description;
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + Objects.hashCode(this.id);
+        hash = 37 * hash + Objects.hashCode(this.username);
+        hash = 37 * hash + Objects.hashCode(this.description);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MathUtil other = (MathUtil) obj;
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    public MathUtil(Long id, String username, String description) {
+        this.id = id;
+        this.username = username;
+        this.description = description;
+    }
+
+    public MathUtil() {
+    }
+    
+    
     
 }
